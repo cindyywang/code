b3d668378cedc7d393de95c9cf128a8fd57781f7
Ying Wang <ying2ra@gmail.com>
2020-07-07 19:02:26 +0800

    implemented evenOrOdd function

---
+++ basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,11 @@
+evenOrOdd = (number) => {
+  // TODO: this should return "even" if the number is even, "odd" otherwise
+  if ( number % 2 == 0 ) {
+    return "even";
+  }
+  else {
+    return "odd";
+  }
+}
+
+console.log(evenOrOdd(3))
